d372cee7a41360d9d5906ef2fbb0ccb59f9bb58d
Riccardo Riva <riccardo.riva.997@gmail.com>
2018-03-18 20:54:27 +0100

    primi passi

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ai
 ai dictionary, some experiment
+
+let's try something cool
+
+1. language detection, I choose Italian (my original language)
--- hello.c
+++ hello.c
@@ -4,17 +4,34 @@
 #include <math.h>
 #include <time.h>
 //#include "hello.h"
-//#include "dictionary.h"
+
+#define TRUE 0
+
 int main(){
 
-	char X[1024];
+	srand(time(NULL));
 
-	sprintf(X, "Ciao, sto imparando, sii clemente (non Smarra)");
+	int i;
+	char X[1024], Y[32];
+	FILE *DIC;
+	DIC = fopen("dictionary.dat", "r");
 
+	sprintf(X, "Ciao");
 	fprintf(stdout, "%s\n", X);
 
-	scanf("%1023s", X);
-	fprintf(stdout, "%s\n", X);
+	do{
+		scanf("%1023s", X);
+	
+		do{
+			fscanf(DIC, "%31s", Y); i++;
+			if(Y == NULL) break;		//checking if it's all right
+			if(strncmp(X, Y, 3) == 0) break; //exit for when comparision is ok
+		} while(TRUE);
+	
+	//fprintf(stdout, "\rusr said:%s\n", Y);
+	
 
+
+	}
 	exit(0);
 }
